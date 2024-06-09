46a1424ada281e923f9458e9a3fbf74b8aaa020c
Nazar <nazarpavl09102004@gmail.com>
2024-06-09 15:25:53 +0200

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import com.example.calculator.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println(add(5,3));
+        System.out.println(difference(5,3));
+        System.out.println(multiply(5,3));
+    }
+}
